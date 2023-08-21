c840d88d714527d484ca4a10ec575a16f7e70d9d
Peraven17 <76138131+Peraven17@users.noreply.github.com>
2023-08-21 17:34:15 +0800

    day16-update_2

--- nodejs-backend/src/mlops/scripts/insert_model.py
+++ nodejs-backend/src/mlops/scripts/insert_model.py
@@ -6,30 +6,30 @@ collection_name = 'modelitems'
 
 client = MongoClient("localhost", 27017)
 db = client[database_name]
-my_collection = db[collection_name]
-model_values = my_collection.find({})
+my_collection = db[collection_name].find({})
 
 classifiers = ['et', 'lr', 'ridge', 'lda', 'xgboost', 'qda', 'nb', 'rf', 'lightgbm', 'gbc', 'ada', 'knn', 'dt', 'dummy', 'svm']
-rows = [[]]
-c = 0
+columns = []
 
-for document in model_values:
-    for col in document:
-        if col == '_id' : continue
-        print(col)
-        print(document[col])
-#         collection = col.replace('.','')
-        d = 14
-        for cls in classifiers:
-            value = document[col][cls]
-            print(c, d, value, col, cls)
+def Merge(dict1, dict2):
+    res = {**dict1, **dict2}
+    return res
 
-            rows.insert(d, {col : value})
-            d -= 1
-            if d==0: d=14
-#             # db[collection_name].insert_one({ col : value})
-#     values.append(rows)
-print(rows)
-# db['model_test'].insert_one(rows)
+for obj in my_collection:
+    for ele in obj:
+        if ele =='_id': continue
+        columns.append(ele)
 
+values = []
+            
+for cls in classifiers:
+    rows = {}
+    for ele in columns:
+        for obj in my_collection.rewind():
+            if ele =='_id': continue
+            dict = {ele : obj[ele][cls]}
+            rows = Merge(rows,dict)
+    db['model_reshaped'].insert_one(rows)
+    values.append(rows)
 
+print(values)
